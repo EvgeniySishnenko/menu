98477fed821050057ad78ecdb17d427c3f7efad5
“Евгений <sishnenko86@mail.ru>
2020-07-05 12:30:33 +0300

    добавил switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TimeAttackPage from "./Components/TimeAttackPage";
 import ForzaPage from "./Components/ForzaPage";
 import DriftPage from "./Components/DriftPage";
@@ -12,10 +12,12 @@ export default function App() {
       <div>
         <Menu />
         <div className="page">
-          <Route path="/" exact component={HomePage} />
-          <Route path="/drift" component={DriftPage} />
-          <Route path="/timeattack" component={TimeAttackPage} />
-          <Route path="/forza" component={ForzaPage} />
+          <Switch>
+            <Route path="/" component={HomePage} />
+            <Route path="/drift" component={DriftPage} />
+            <Route path="/timeattack" component={TimeAttackPage} />
+            <Route path="/forza" component={ForzaPage} />
+          </Switch>
         </div>
       </div>
     </Router>
